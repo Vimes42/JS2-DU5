0e8e620e2613765bc97bc69ea466d7afd05444ae
Lenka Polcerova <lenka.polcerova@gmail.com>
2024-06-05 11:06:29 +0200

    dú hotov

--- src/pages/components/city/city.jsx
+++ src/pages/components/city/city.jsx
@@ -1,63 +1,14 @@
-import './city.css'
-const cities = [
-  {
-    name: 'Praha',
-    population: 1324277,
-    area: 496.21,
-  },
-  {
-    name: 'Brno',
-    population: 381346,
-    area: 230.18,
-  },
-  {
-    name: 'Ostrava',
-    population: 287968,
-    area: 214.23,
-  },
-  {
-    name: 'Plzeň',
-    population: 174842,
-    area: 137.67,
-  },
-  {
-    name: 'Liberec',
-    population: 104802,
-    area: 106.09,
-  },
-  {
-    name: 'Olomouc',
-    population: 100663,
-    area: 103.33,
-  },
-  {
-    name: 'České Budějovice',
-    population: 94463,
-    area: 55.6,
-  },
-  {
-    name: 'Hradec Králové',
-    population: 92939,
-    area: 105.69,
-  },
-  {
-    name: 'Ústí nad Labem',
-    population: 92716,
-    area: 93.97,
-  },
-  {
-    name: 'Pardubice',
-    population: 91727,
-    area: 82.66,
-  },
-];
+import './city.css';
+import { cities } from '../../../cz-cities';
 
-
-const CityItem = ({name, population, area}) => (
+const CityItem = ({name, population, area, district, photo }) => (
         <div className="city">
+           
            <h2>Město: {name}</h2>
+           <img src={photo} alt={name} />
            <p>Počet obyvatel: {population}</p>
            <p>Rozloha: {area} km2 </p>
+           <p>{district} </p>
          </div>
 );
 
@@ -65,7 +16,7 @@ export const City = () => (
     <>
     <h1>Seznam měst</h1>
     <div className="citiesList">
-      {cities.map((city) => (<CityItem key={name} name={city.name} population={city.population} area={city.area} />)
+      {cities.map((city) => (<CityItem key={name} name={city.name} photo={city.photo} population={city.population} area={city.area} district={city.district} />)
       )}
     </div>
     </>
